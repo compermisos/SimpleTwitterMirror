5056b8992eca4103be401fee44b99c4f957321d1
Jesus Christian Cruz Acono <jccruz@openintelligence.mx>
2012-05-27 03:12:19 -0500

    added a configuration zone =) and polish internal code

--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <?php
+
+#######################################################################
+#                         config zone                                 #
+#######################################################################
+$CacheDir = './cache/';
+$CacheTime = 300;
+$DefaultUser = "JesusCCruz";
+
+$TwitterBaseURL = "https://twitter.com/statuses/user_timeline/";
+/*yes you can make a chain of proxys =P */
+$TwitterCloseURL = ".xml?count=10";
+
+
+
+#######################################################################
+#                         end config zone                             #
+#######################################################################
 include("JG_cache.php");
 
-$cache = new JG_Cache('./cache/');  
+$cache = new JG_Cache($CacheDir);  
 if(isset($_GET['user'])){
 	$user = $_GET['user'];
 }else{
-	$user = "JesusCCruz";
+	$user = $DefaultUser;
 }
-$data = $cache->get($user, 300);  
+$data = $cache->get($user, $CacheTime);  
   
 if ($data === FALSE)  
 {  
-	$url = "https://twitter.com/statuses/user_timeline/" . $user . ".xml?count=10";
+	$url = $TwitterBaseUR . $user . $TwitterCloseURL;
 	$data = file_get_contents($url);
 	$cache->set($user, $data);  
 }  
   
 echo $data;
-?>
